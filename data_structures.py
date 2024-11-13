87295e600dcc6d288a23596eb34e4fd794e10f38
Nathananiel26 <yarfohnat2@gmail.com>
2024-11-13 10:13:31 +0000

    commit

--- data_structures.py
+++ data_structures.py
@@ -105,3 +105,71 @@ list_num_2 = [5, 7, 8]
 # var = class_records.keys()
 # for each_item in class_records:
 #     print(class_records[each_item])
+
+#------------------------------------------FUNCTIONS---------------------------
+#it is a block of reuseable code used to perform a simple related action
+
+# def addition(num_1,num_2):
+#     result = num_1 + num_2
+#     #print(result)
+#     return result
+
+# add = addition(3,8) 
+# print(f"the sum of the numbers is {add}")
+
+# #----------------------------------------------
+
+# def subtraction():
+#     num_3 = int(input("enter first number: "))
+#     num_4 = int(input("enter second number"))
+#     answer = num_3  - num_4
+#     print(answer)
+#     return answer
+
+# subtraction()
+
+# #------------------------------------------------
+# def division(num_5,num_6):
+#     render= num_5 / num_6
+#     return render
+# divide = division(50,5)
+# print(f"the division of the two numbers is {divide}")
+
+def user_inputs():
+    user_input_1 = int(input("enter the sales amount: "))
+    user_input_2 = float(input("enter the commission rate "))
+    return user_input_1 , user_input_2
+
+num_1 , num_2 = user_inputs()
+
+def sales_commission(num_1,num_2):
+    result = num_1 * num_2
+    return result
+    
+commission = sales_commission(num_1,num_2) 
+print(f"your sale commission is : {commission}")
+
+
+####-----------------------------------------------
+#mkdir = is used to create a directory
+#touch = used to create a file in a folder
+#pandas and numpy is for data processing
+#sea born is for data visualization
+
+# pip --version
+# pip --help
+# pip freeze
+# pip install
+# pip download
+# pip install pandas== 2.0.0 (used to install a specific version of a package)
+
+##--------------------Virtual Environment----------------
+#python -m venv myenv (this creates a virtual environment in your repository)
+#clone your repository
+# create your virtual environment (python -m venv myenv)
+#to check if the environment is activated (source myenv/Scripts/activate)
+# you can install your packages
+# jupyter-notebook(to start the package)
+#shutdown the kernel in the browser
+# use ctrl in the terminal to interrupt
+#use "deactivate" in terminal to close the environment
